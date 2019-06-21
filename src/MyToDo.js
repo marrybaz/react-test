68b8845fa0c39fd85e9db5987f8e1d65005a6afd
Marija Bazovic <mbazovic@live.com>
2019-06-21 06:22:35 +0200

    giddle

--- src/MyToDo.js
+++ src/MyToDo.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ToDoItem from './ToDoItem'
 import ItemData from './ItemData'
+import Griddle, { plugins, RowDefinition, ColumnDefinition } from 'griddle-react';
 
 function MyToDo() {
     const data = ItemData.map(item => <ToDoItem name={item.name} content={item.content} key={item.id} />)
     return (
         <div>
             {data}
+            <Griddle
+    data={ItemData}
+    plugins={[plugins.LocalPlugin]}
+  />
         </div>
     )
 
